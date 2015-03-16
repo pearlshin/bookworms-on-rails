d3db4c1f407d2f88c61f118926ae0a4c7a88fc40
Pearl Shin <pearlshin22@gmail.com>
2015-03-15 23:33:39 -0400

    add initial user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,30 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :show, :update, :destroy]
   def index
     @users = User.all
-  end
-
-  def login
-    # @user = User.find(params[:id])
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :pic_url, :password, :password_confirmation)
+  end
+
 
 
 end
